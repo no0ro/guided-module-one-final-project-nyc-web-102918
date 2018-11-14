99955bfe89fffb615e350128aded86bcd5cd5112
no0ro <nrolwes@gmail.com>
2018-11-14 14:12:07 -0500

    models

---
+++ lib/song.rb
@@ -0,0 +1,3 @@
+class Song < ActiveRecord::Base
+  has_many :songPlaylists
+  has_many :playlists, through: song_playlist
---
+++ lib/songPlaylist.rb
@@ -0,0 +1,3 @@
+class SongPlaylist < ActiveRecord::Base
+belongs_to :song
+belongs_to :playlist
