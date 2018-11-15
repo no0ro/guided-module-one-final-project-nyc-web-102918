0cc166b63f200ce1ab9d537ea34d9c194622b640
HannaStrezh <hannastrezh@gmail.com>
2018-11-15 14:45:10 -0500

    connect faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,29 @@
+# require 'faker'
 # s1 = Song.new(name: "blah")
 # s1.save
 #same as
 
-s1 = Song.create(name: 'blah')
+# s1 = Song.create(name: 'a')
+# s1 = Song.create(name: 'b')
+# s1 = Song.create(name: 'c')
+# s1 = Song.create(name: 'd')
+# s1 = Song.create(name: 'e')
+# s1 = Song.create(name: 'f')
+# s1 = Song.create(name: 'g')
+# s1 = Song.create(name: 'g')
+# s1 = Song.create(name: 'g1')
+# s1 = Song.create(name: 'r4')
+# s1 = Song.create(name: 'bg')
+
+
+10.times do
+  Song.create(name: Faker::Name.name)
+end
+
+10.times do
+  Playlist.create(name: Faker::Name.name)
+end
+
+10.times do
+  SongPlaylist.create(song_id: Song.all.sample.id, playlist_id: Playlist.all.sample.id)
+end
--- lib/songPlaylist.rb
+++
@@ -1,7 +0,0 @@
-class SongPlaylist < ActiveRecord::Base
-belongs_to :song
-belongs_to :playlist
-
-
-
-end# end of Song class
